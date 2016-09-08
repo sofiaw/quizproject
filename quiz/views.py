0ed00c318cde6d24c95c22149dc0480bf3701e38
sofiaw <sofiag@Sofias-MacBook-Pro.local>
2016-09-08 12:49:20 +0200

    ändra definitionen av completed

--- quiz/views.py
+++ quiz/views.py
@@ -38,6 +38,11 @@ def question(request, quiz_number, question_number):
 	    "quiz_number": quiz_number,
 		}
 	return render(request, "quiz/question.html", context)
-def completed(request):
-	return render(request, "quiz/results.html")
+def completed(request, quiz_number):
+	context = {
+		"correct": 12,
+	    "total": 20,
+		"quiz_number": quiz_number,
+	}
+	return render(request, "quiz/results.html",context)
 
