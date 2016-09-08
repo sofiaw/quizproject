0e6aaa4cb91929aa9db06304a901a85fcb60866e
sofiaw <sofiag@Sofias-MacBook-Pro.local>
2016-09-08 12:26:24 +0200

    Ändra definitionen av quiz i quiz/views.py

--- quiz/views.py
+++ quiz/views.py
@@ -22,8 +22,12 @@ def startpage(request):
 		"quizzes":quizzes
 	}
 	return render(request,"quiz/start.html",context)
-def quiz(request):
-	return render(request,"quiz/quiz.html")
+def quiz(request, quiz_number):
+	context = {
+		"quiz":quizzes[int(quiz_number)-1],
+		"quiz_number":quiz_number,
+	}
+	return render(request,"quiz/quiz.html", context)
 def question(request):
 	return render(request, "quiz/question.html")
 def completed(request):
