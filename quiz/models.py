ba41139a28431b8fa85d41fff1eb80659b9f1894
sofiaw <sofiag@Sofias-MacBook-Pro.local>
2016-09-09 11:20:06 +0200

    spökande komma

--- quiz/models.py
+++ quiz/models.py
@@ -10,9 +10,11 @@ class Quiz(models.Model):
 
 
 class Question(models.Model):
-	question = models.TextField(),
+	question = models.TextField()
 	answer1 = models.CharField(max_length=100)
 	answer2 = models.CharField(max_length=100)
 	answer3 = models.CharField(max_length=100)
 	correct = models.PositiveIntegerField()
 	quiz = models.ForeignKey(Quiz, related_name="questions")
+	def __str__(self):
+		return self.quiz.name + " / " + self.question
