2d6a8f349fbef2704303c88ff6e6433dde9c6a7b
sofiaw <sofiag@Sofias-MacBook-Pro.local>
2016-09-09 10:54:46 +0200

    migrering igen ändringar databas

--- quiz/models.py
+++ quiz/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Quiz(models.Model):
-	quiz_number = models.PositiveIntegerField(),
-	name = models.CharField(max_length=100),
-	description = models.TextField(),
+	quiz_number = models.PositiveIntegerField()
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	def __str__(self):
+		return self.name
+
 
 class Question(models.Model):
 	question = models.TextField(),
-	answer1 = models.CharField(max_length=100),
-	answer2 = models.CharField(max_length=100),
-	answer3 = models.CharField(max_length=100),
-	correct = models.PositiveIntegerField(),
+	answer1 = models.CharField(max_length=100)
+	answer2 = models.CharField(max_length=100)
+	answer3 = models.CharField(max_length=100)
+	correct = models.PositiveIntegerField()
 	quiz = models.ForeignKey(Quiz, related_name="questions")
