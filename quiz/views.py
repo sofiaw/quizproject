ea4d5ed398390e1865bfac94b58a11818009b512
sofiaw <sofiag@Sofias-MacBook-Pro.local>
2016-09-09 11:41:30 +0200

    question django koppling

--- quiz/views.py
+++ quiz/views.py
@@ -9,8 +9,8 @@ def startpage(request):
 	return render(request,"quiz/start.html",context)
 def quiz(request, quiz_number):
 	context = {
-		"quiz":quizzes[int(quiz_number)-1],
-		"quiz_number":quiz_number,
+		"quiz": Quiz.objects.get(quiz_number=quiz_number),
+		"quiz_number": quiz_number,
 	}
 	return render(request,"quiz/quiz.html", context)
 def question(request, quiz_number, question_number):
